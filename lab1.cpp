b20ae2f3195ef45136b7521f8e7217915b13dbd6
kingular022 <92390717+kingular022@users.noreply.github.com>
2022-01-24 20:33:33 +0100

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	zad1
+	string nr_albumu = "w65537";
+
+	cout << "Nr albumu " << nr_albumu << endl;
+
+	zad2
+	int a;
+	cin >> a;
+	if (a % 2 == 0)
+		cout << "jest parzysta\n";
+	else
+		cout << "nie jest parzysta\n";
+
+	zad3
+	float a, b;
+	cout << "podaj a: " << endl;
+	cin >> a;
+	cout << "podaj b: " << endl;
+	cin >> b;
+
+	if (a == b)
+		cout << "liczby sa rowne" << endl;
+	else if (a > b)
+		cout << "a jest wieksze od b" << endl;
+	else
+		cout << "b jest wieksze od a" << endl;
+
+	zad4
+	float a, b, c, d;
+	cout << "podaj a: " << endl;
+	cin >> a;
+	cout << "podaj b: " << endl;
+	cin >> b;
+	cout << "podaj c: " << endl;
+	cin >> c;
+	cout << "podaj d: " << endl;
+	cin >> d;
+
+	cout << "srednia arytmetyczna to: " << (a + b + c + d) / 4 << endl;
+
+	zad5
+	double a, b;
+		int wybor;
+		cout << "podaj a: ";
+		cin >> a;
+		cout << "podaj b: ";
+		cin >> b;
+
+		cout << endl;
+		cout << "Wybierz dzialanie" << endl;
+		cout << endl;
+		cout << "1. suma" << endl;
+		cout << "2. roznica" << endl;
+		cout << "3. iloczyn" << endl;
+		cout << "4. iloraz" << endl;
+		cout << endl;
+
+		cin >> wybor;
+
+		switch (wybor)
+		{
+		case 1:
+			cout << "suma: " << a + b << endl;
+			break;
+		case 2:
+			cout << "roznica: " << a - b << endl;
+			break;
+		case 3:
+			cout << "iloczyn: " << a * b << endl;
+			break;
+		case 4:
+			if (b == 0)
+				cout << "nie mozna dzielic przez zero";
+			else
+				cout << "iloraz: " << a / b << endl;
+			break;
+		default:
+			cout << " nie ma takiej opcji" << endl;
+		}
+	
+	*/
+}
